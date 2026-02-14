ff2b398c515b13f218d316d4c29f7b5fffe7380f
TubzDaBubz <JEREBITO13@GMAIL.COM>
2026-02-14 16:30:40 -0500

    week3prog_
    lots of shi

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{ 
+    string variable2 = "Hello ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        string variable1 = "World";
+        Debug.Log(variable2 + variable1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
