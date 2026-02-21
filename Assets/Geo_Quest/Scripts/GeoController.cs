e25439d742097e3e8255fdd24dfcbf5f26b7094f
TubzDaBubz <JEREBITO13@GMAIL.COM>
2026-02-21 16:24:05 -0500

    fahhh
    dihhhh

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class GeoController : MonoBehaviour
 { 
     string variable2 = "Hello ";
+    private int firstNum = 3;
     // Start is called before the first frame update
     void Start()
     {
         string variable1 = "World";
         Debug.Log(variable2 + variable1);
+
+
     }
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Debug.Log(firstNum++);
+
+        if (Input.GetKeyDown(KeyCode.W)) ;
+        transform.position+= new Vector3(0, 5, 0);
     }
 }
